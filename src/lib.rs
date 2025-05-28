54fba2ab449cdb644e1b1c0c1fcf5eecef5ac66b
William Stoneham <william.stoneham04@gmail.com>
2025-05-28 19:20:07 +0100

    got it working in both environments

--- src/lib.rs
+++ src/lib.rs
@@ -11,36 +11,28 @@ pub fn lambda_function(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let expanded = quote! {
         #input_fn
 
-        #[tokio::main]
-        async fn main() -> Result<(), lambda_runtime::Error> {
-            let func = lambda_runtime::service_fn(#fn_name);
-            lambda_runtime::run(func).await?;
-            Ok(())
-        }
-    };
-
-    TokenStream::from(expanded)
-}
-
-#[proc_macro_attribute]
-pub fn local_function(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input_fn = parse_macro_input!(item as ItemFn);
-    let fn_name = &input_fn.sig.ident;
-
-    let expanded = quote! {
-        #input_fn
-
-
-        fn generate_default_lambda_event() -> lambda_runtime::LambdaEvent<Value> {
+        fn generate_default_lambda_event() -> lambda_runtime::LambdaEvent<serde_json::Value> {
             lambda_runtime::LambdaEvent::new(serde_json::Value::default(), lambda_runtime::Context::default())
         }
 
         #[tokio::main]
         async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-            let event = generate_default_lambda_event();
-            let result = #fn_name(event).await?;
+            // Check if running in lambda environment
+            let is_lambda = std::env::vars()
+                .any(|(key, _)| key.starts_with("AWS_LAMBDA"));
 
-            print!("{:?}", result);
+            println!("Running in Lambda environment: {}", is_lambda);
+
+
+            if is_lambda {
+                let func = lambda_runtime::service_fn(#fn_name);
+                lambda_runtime::run(func).await?;
+            } else {
+                let event = generate_default_lambda_event();
+                let result = #fn_name(event).await?;
+                print!("{:?}", result);
+            }
+
             Ok(())
         }
     };
