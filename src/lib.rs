e01b4747419136fb775e61e22b1f3b848c12e599
William Stoneham <william.stoneham04@gmail.com>
2025-06-18 18:03:05 +0100

    added back local function, just for debugging as debugger keeps crashing

--- src/lib.rs
+++ src/lib.rs
@@ -39,3 +39,29 @@ pub fn lambda_function(_attr: TokenStream, item: TokenStream) -> TokenStream {
 
     TokenStream::from(expanded)
 }
+
+#[proc_macro_attribute]
+pub fn local_function(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input_fn = parse_macro_input!(item as ItemFn);
+    let fn_name = &input_fn.sig.ident;
+
+    let expanded = quote! {
+        #input_fn
+
+
+        fn generate_default_lambda_event() -> lambda_runtime::LambdaEvent<Value> {
+            lambda_runtime::LambdaEvent::new(serde_json::Value::default(), lambda_runtime::Context::default())
+        }
+
+        #[tokio::main]
+        async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+            let event = generate_default_lambda_event();
+            let result = #fn_name(event).await?;
+
+            print!("{:?}", result);
+            Ok(())
+        }
+    };
+
+    TokenStream::from(expanded)
+}
